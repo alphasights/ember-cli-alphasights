b51eea443230744ec0cbc1c7db0c61e43b75b024
Ju Liu <ju.liu@welaika.com>
2015-01-16 11:48:55 +0000

    Test globbing

--- blueprints/alphasights/index.js
+++ blueprints/alphasights/index.js
@@ -1,10 +1,20 @@
 var fs = require('fs');
 var path = require('path');
+var glob = require('glob');
 
 module.exports = {
   normalizeEntityName: function() {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
+  },
+
+  afterInstall: function() {
+    glob('bin/*', function(er, files) {
+      files.forEach(function(el) {
+        console.log(el);
+        fs.chmodSync(el, '0755')
+      })
+    });
   }
 }
